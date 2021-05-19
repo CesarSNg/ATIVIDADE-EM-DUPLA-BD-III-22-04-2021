06d6f0ff998a4b06e2742e6967666cf58e92fe63
César Ng <csantanang@gmail.com>
2021-05-18 21:42:53 -0300

    Atividade banco de dados com criação das tabelas

--- Atividade.sql
+++ Atividade.sql
@@ -1,26 +1,55 @@
-CREATE DATABASE UNIVERSIDADE;
-
-create table Professores (
-matricula int,
-nome varchar(40) not null,
-RG numeric(10) unique,
-sexo char(1) check(sexo in ('M','F')),
-idade tinyint check(idade between 21 and 80),
-titulacao varchar(15) check(titulacao in ('graduado','especialista', 'mestre', 'doutor')),categoria varchar(15) check(categoria in ('auxiliar','assistente', 'adjunto', 'titular')),nroTurmas tinyint check(nroTurmas >= 0),
-constraint pkProf primary key (matricula))
-
-CREATE TABLE Cursos (
-codigo PRIMARY KEY,
-nome varchar(40) not null,
-duracao tinyint check(idade between 4 and 12),
-coordenador constraint fkProf foreign key (coordenador) references Professores on update cascade)
-
-CREATE TABLE Disciplinas (
-codigo PRIMARY KEY,
-nome varchar(40) NOT NULL,
-creditos tinyint check(idade between 2 and 8))
+ CREATE DATABASE UNIVERSIDADE;
+USE UNIVERSIDADE;
 
 CREATE TABLE Curriculos (
-curso CONSTRAINT fkCod FOREIGN KEY (codigo) REFERENCES Cursos ON UPDATE CASCADE,
---Teste
-)
+  codigo_curso INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  codigo_disciplina INTEGER UNSIGNED NOT NULL,
+  Disciplina_codigo_disciplina INTEGER UNSIGNED NOT NULL,
+  Cursos_codigo_curso INTEGER UNSIGNED NOT NULL,
+  vagas INT NULL,
+  matricula INT NULL,
+  PRIMARY KEY(codigo_curso, codigo_disciplina),
+  INDEX Curriculos_FKIndex1(Cursos_codigo_curso),
+  INDEX Curriculos_FKIndex2(Disciplina_codigo_disciplina)
+);
+
+CREATE TABLE Cursos (
+  codigo_curso INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  Professores_matricula INT NOT NULL,
+  nome VARCHAR(15) NULL,
+  duracao TINYINT UNSIGNED NULL,
+  matricula INT NULL,
+  PRIMARY KEY(codigo_curso),
+  INDEX Cursos_FKIndex1(Professores_matricula)
+);
+
+CREATE TABLE Disciplina (
+  codigo_disciplina INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(15) NULL,
+  creditos CHAR(1) NULL,
+  PRIMARY KEY(codigo_disciplina)
+);
+
+CREATE TABLE Professores (
+  matricula INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(40) NULL,
+  RG NUMERIC(10) NULL,
+  sexo CHAR(1) NULL,
+  idade TINYINT UNSIGNED NULL,
+  titulacao VARCHAR(15) NULL,
+  categoria VARCHAR(15) NULL,
+  nroTurmas TINYINT UNSIGNED NULL,
+  PRIMARY KEY(matricula)
+);
+
+CREATE TABLE Turmas (
+  codigo_turma INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  codigo_disciplina INTEGER UNSIGNED NOT NULL,
+  Disciplina_codigo_disciplina INTEGER UNSIGNED NOT NULL,
+  Professores_matricula INT NOT NULL,
+  vagas INT NULL,
+  matricula INT NULL,
+  PRIMARY KEY(codigo_turma, codigo_disciplina),
+  INDEX Turmas_FKIndex1(Professores_matricula),
+  INDEX Turmas_FKIndex2(Disciplina_codigo_disciplina)
+);
