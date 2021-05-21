cbfa365161820b8f58ff0994d6c0cd7cd98d9d17
César Ng <csantanang@gmail.com>
2021-05-20 21:56:35 -0300

    Atividade final (Triggers)

--- Atividade.sql
+++ Atividade.sql
@@ -53,3 +53,34 @@ CREATE TABLE Turmas (
   INDEX Turmas_FKIndex1(Professores_matricula),
   INDEX Turmas_FKIndex2(Disciplina_codigo_disciplina)
 );
+
+INSERT INTO Curriculos(codigo_disciplina, vagas, matricula)
+VALUES 
+(1, 73, 14052004),
+(2, 105, 11011966);
+
+INSERT INTO Cursos(nome, duracao, matricula)
+VALUES 
+('Matematica', 18, 987654321),
+('Português', 8, 123456789);
+
+INSERT INTO Disciplina(nome, creditos)
+VALUES
+('Banco de dados', 6), 
+('Sistemas Embarcados', 8);
+
+INSERT INTO Professores(nome, RG, sexo, idade, titulacao, categoria, nroTurmas)
+VALUES
+('César', 45139879875, 'M', 17, 'mestre', 'Professor de exatas', 3), 
+('Thigaas', 03540327045, 'M', 22, 'mestre', 'Professor de humanas', 5);
+
+INSERT INTO Turmas(vagas, matricula)
+VALUES 
+(60, 11223344),
+(60, 99887766);
+
+
+
+create trigger IncTurmasProfonTurmas after insert as update Professores 
+set nroTurmas = nroTurmas + 1 where exists(
+select professor from inserted where Professores.matricula = professor);
